236affb3703f8da32c238b4ed75bb392a1085adf
Lucas Vinicius <lucasviniciuscv10@gmail.com>
2024-09-11 20:19:17 -0300

    While/Switch

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const start = () => {
-    let count = 3
-    while(count <= 10){
-        console.log(count)
-        count = count + 1 
+    
+    while(true){
+        let opção = "Cadastrar"
+        switch(opção) {
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "Sair":
+                return
+        }
     }
 } 
 
